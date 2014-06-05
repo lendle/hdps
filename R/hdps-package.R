aae22a4785a7d88d38994a502be1d2bdf1258cb7
Sam Lendle <sam.lendle@gmail.com>
2014-06-05 12:04:32 -0700

    Use roxygenize

---
+++ R/hdps-package.R
@@ -0,0 +1,38 @@
+
+
+#' High-dimensional propensity score algorithm
+#' 
+#' The high-dimensional propensity score algorithm is a method for
+#' high-dimensional proxy adjustment in claims data. This package implements
+#' the variable transformation and varaible selection parts of the whole
+#' algorithm.
+#' 
+#' \tabular{ll}{
+#' Package: \tab hdps\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.0\cr
+#' Date: \tab 2014-06-05\cr
+#' License: \tab MIT \cr 
+#' } ~~ An overview of
+#' how to use the package, including the most important functions ~~
+#' 
+#' @name hdps-package
+#' @aliases hdps-package hdps
+#' @docType package
+#' @author Sam Lendle
+#' 
+#' Maintainer: Sam Lendle <sam.lendle@@gmail.com>
+#' 
+#' @references Schneeweiss, S., Rassen, J. A., Glynn, R. J., Avorn, J., Mogun,
+#' H., & Brookhart, M. A. (2009). High-dimensional propensity score adjustment
+#' in studies of treatment effects using health care claims data. Epidemiology
+#' (Cambridge, Mass.), 20(4), 512.
+#' @keywords package
+#' @examples
+#' 
+#' #~~ simple examples of the most important functions ~~
+#' 
+NULL
+
+
+
