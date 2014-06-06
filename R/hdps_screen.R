93d395b9262225fbd51fb24bf9345a105505d157
Sam Lendle <sam.lendle@gmail.com>
2014-06-05 18:14:41 -0700

    Added verbose option and an example.

--- R/hdps_screen.R
+++ R/hdps_screen.R
@@ -39,16 +39,35 @@
 #' @param dimension_indexes A list of vectors of column indexes corresponding to dimension groups. See details. Cannot be specified with \code{dimension_names}.
 #' @param keep_n_per_dimension The maximum number of covariates to be kept per dimension by \code{\link{identify_covariates}}.
 #' @param keep_k_total Total number of covariates to keep after expanding by \code{\link{assess_recurrence}} and ordering by \code{link{prioritize_covariates}}.
+#' @param verbose Should verbose output be printed?
 #' @return Matrix of filtered and expanded covariates. 
 #' @references Schneeweiss, S., Rassen, J. A., Glynn, R. J., Avorn, J., Mogun,
 #' H., & Brookhart, M. A. (2009). High-dimensional propensity score adjustment
 #' in studies of treatment effects using health care claims data. \emph{Epidemiology
 #' (Cambridge, Mass.)}, 20(4), 512.
 #' @author Sam Lendle
+#' @examples
+#' set.seed(123)
+#' n <- 1000
+#' p <- 10000
+#' out <- rbinom(n, 1, 0.05)
+#' trt <- rbinom(n, 1, 0.5)
+#' covars <- matrix(rbinom(n*p, 3, 0.05), n)
+#' colnames(covars) <- c(paste("drug", 1:(p/2), sep="_"),
+#'                       paste("proc", 1:(p/2), sep="_"))
+#' 
+#' dimension_names <- c("drug", "proc")
+#' 
+#' screened_covars <- hdps_screen(out, trt, covars, 
+#'                                dimension_names = dimension_names,
+#'                                keep_n_per_dimension = 400,
+#'                                keep_k_total = 200,
+#'                                verbose=TRUE)
 #' @export
 hdps_screen <- function(outcome, treatment, covars,
                         dimension_names=NULL, dimension_indexes=NULL,
-                        keep_n_per_dimension=200, keep_k_total=500) {
+                        keep_n_per_dimension=200, keep_k_total=500,
+                        verbose=FALSE) {
   
   check_inputs(outcome, treatment, covars)
   
@@ -58,7 +77,7 @@ hdps_screen <- function(outcome, treatment, covars,
   
   if (!is.null(dimension_names)) {
     dimension_indexes <- lapply(dimension_names, grep, x = colnames(covars))
-    all_idx <- do.call(c, dimension_names)
+    all_idx <- do.call(c, dimension_indexes)
     if (anyDuplicated(all_idx)) {
       stop("Some column names of covars are matched by more than one pattern in dimension_names")
     }
@@ -69,14 +88,23 @@ hdps_screen <- function(outcome, treatment, covars,
   
   # Step 2. Identify empirical candidate covariates
   if (is.null(dimension_indexes)) {
+    if (verbose) message("No dimensions specified...")
+    if (verbose) message("Filtering covariates...")
     filtered_covars <- identify_covariates(covars, keep_n_covars=keep_n_per_dimension, indexes=FALSE)
   } else {
-    filtered_covars <- lapply(dimension_indexes, identify_covariates, covars,
-                                  keep_n_covars=keep_n_per_dimension, indexes=FALSE)
-    filtered_covars <- do.call(rbind, filtered_covars)
+    if (verbose) message("Filtering covariates...")
+    filtered_covars <- lapply(seq_along(dimension_indexes), function(i) {
+      if (verbose) message("\tFiltering dimension ", 
+                           if (!is.null(dimension_names)) dimension_names[i] else i,
+                           "...")
+      identify_covariates(covars, keep_n_covars=keep_n_per_dimension, indexes=FALSE)
+    })
+    if (verbose) message("Combining dimensions...")
+    filtered_covars <- do.call(cbind, filtered_covars)
   }
   
   #Step 3. Assess recurrence
+  if (verbose) message("Expanding covariates...")
   expanded_covars <- assess_recurrence(filtered_covars)
   
   if (ncol(expanded_covars) <= keep_k_total) {
@@ -84,12 +112,14 @@ hdps_screen <- function(outcome, treatment, covars,
   }
   
   #Step 4. Prioritize covariates
+  if (verbose) message("Prioritizing covariates...")
   ordered_indexes <- prioritize_covariates(outcome, treatment, expanded_covars, keep_NaNs=TRUE)
   
   #(Part of) Step 5. Select covariates
   selected_indexes <- ordered_indexes[1:min(keep_k_total, length(ordered_indexes))]
   selected_covars <- expanded_covars[, sort(selected_indexes)]
   
+  if (verbose) message("...Done!")
   return(selected_covars)
 }
   
